39088aac069267956a812b8dfc0a2a288532c146
al3jorodriguez <al3jorodriguez@gmail.com>
2024-05-25 02:53:37 +0200

    Update index.js
    Update routes

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use(cors());
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '10mb', parameterLimit: 50000 }));
+
+app.use('/api/external/monitoring', require('./api/routes/external'));
+app.use('/api/internal', require('./api/routes/internal'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
 
 module.exports = app;
