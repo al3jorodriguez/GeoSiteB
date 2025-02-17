81a21e2d5472b631487ba83c96b1343bbb85975f
Alejandro Rodriguez <al3jorodriguez@gmail.com>
2025-02-18 00:44:31 +0100

    email service implemented, action page implemented, environment variables implemented

--- api/routes/external.js
+++ api/routes/external.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const cache = require("memory-cache");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+
+dotenv.config(); // Cargar variables de entorno desde .env
 
 const { getList, getCountryAndSites } = require("../services/list");
 const { getById } = require("../services/byId");
 const { getXmlInfo } = require("../services/utils");
 
-const CACHE_DURATION = 36000000;
+const CACHE_DURATION = Number(process.env.CACHE_DURATION);
 
 router.get("/xml", async (req, res) => {
   try {
@@ -63,6 +67,38 @@ router.get("/details/:id/:lang", async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json(error.message);
+  }
+});
+
+router.post("/send-email", async (req, res) => {
+  const { name, surname, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST, // Servidor SMTP de Infomaniak
+    port: Number(process.env.SMTP_PORT), // Usa 465 para SSL o 587 para STARTTLS
+    secure: process.env.SMTP_SECURE === "true", // true para SSL (465), false para STARTTLS (587)
+    auth: {
+      user: process.env.SMTP_USER, // Tu correo en Infomaniak
+      pass: process.env.SMTP_PASS, // La contraseña del correo
+    },
+    tls: {
+      rejectUnauthorized: false, // Evita errores con certificados SSL/TLS
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMTP_USER,
+    to: process.env.SMTP_CONTACT,
+    subject: `${name} ${surname}`,
+    text: `${message} email ${email}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Email sent" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error sending email" });
   }
 });
 
--- api/routes/internal.js
+++ api/routes/internal.js
@@ -1,129 +1,172 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/menu', async (req, res) => {
-    try {
-        return res.status(200).json([
-            {
-                id: 'about',
-                label: 'About',
-                translate: 'MENU.ABOUT.ROOT',
-                route: '/about',
-                children: [
-                    {
-                        label: 'Our Vision',
-                        translate: 'MENU.ABOUT.VISION',
-                        route: '/about',
-                        queryParams: { section: 'our-vision' },
-                    },
-                    {
-                        label: 'Invest in Nature',
-                        translate: 'MENU.ABOUT.INVEST',
-                        route: '/about',
-                        queryParams: { section: 'invest-in-nature' },
-                    },
-                    {
-                        label: 'Ecosystems',
-                        translate: 'MENU.ABOUT.ECOSYSTEMS',
-                        route: '/about',
-                        queryParams: { section: 'ecosystems' },
-                    },
-                    {
-                        label: 'Metrics',
-                        translate: 'MENU.ABOUT.METRICS',
-                        route: '/about',
-                        queryParams: { section: 'metrics' },
-                    },
-                ],
-            },
-            {
-                id: 'community',
-                label: 'Community',
-                translate: 'MENU.COMMUNITY.ROOT',
-                route: '/community',
-                children: [
-                    {
-                        route: '/community',
-                        translate: 'MENU.COMMUNITY.NETWORK',
-                        label: 'The Network',
-                        queryParams: { section: 'network' },
-                    },
-                    {
-                        route: '/community',
-                        translate: 'MENU.COMMUNITY.LABORATORIES',
-                        label: 'Laboratories',
-                        queryParams: { section: 'laboratories' },
-                    },
-                    {
-                        route: '/community',
-                        translate: 'MENU.COMMUNITY.FORUM',
-                        label: 'Forum',
-                        queryParams: { section: 'forum' },
-                    },
-                ],
-            },
-            {
-                id: 'technology',
-                label: 'Technology',
-                translate: 'MENU.TECHNOLOGY.ROOT',
-                route: '/technology',
-                children: [
-                    {
-                        route: '/technology',
-                        translate: 'MENU.TECHNOLOGY.EDNA',
-                        label: 'eDNA',
-                        queryParams: { section: 'edna' },
-                    },
-                    {
-                        route: '/technology/',
-                        translate: 'MENU.TECHNOLOGY.REMOTE_SENSING',
-                        label: 'Remote sensing',
-                        queryParams: { section: 'remote-sensing' },
-                    },
-                    {
-                        route: '/technology',
-                        translate: 'MENU.TECHNOLOGY.PROTOCOLS',
-                        label: 'Protocols',
-                        queryParams: { section: 'protocols' },
-                    },
-                    {
-                        route: '/technology',
-                        translate: 'MENU.TECHNOLOGY.BIOBANK',
-                        label: 'Biobank',
-                        queryParams: { section: 'biobank' },
-                    },
-                ],
-            },
-            {
-                id: 'resources',
-                label: 'Resources',
-                translate: 'MENU.RESOURCES.ROOT',
-                route: '/resources',
-                children: [
-                    {
-                        route: '/resources',
-                        translate: 'MENU.RESOURCES.DATA_SHARING',
-                        label: 'Data sharing',
-                        queryParams: { section: 'data-sharing' },
-                    },
-                    {
-                        route: '/resources',
-                        translate: 'MENU.RESOURCES.SINEWS',
-                        label: 'Resources',
-                        queryParams: { section: 'sinews' },
-                    },
-                    {
-                        route: '/resources',
-                        translate: 'MENU.RESOURCES.PUBLICATIONS',
-                        label: 'Publications',
-                        queryParams: { section: 'publications' },
-                    },
-                ],
-            },
-        ]);
-    } catch (e) {
-        return res.status(500).json(error.message);
-    }
+router.get("/menu", async (req, res) => {
+  try {
+    return res.status(200).json([
+      {
+        id: "about",
+        label: "About",
+        translate: "MENU.ABOUT.ROOT",
+        route: "/about",
+        children: [
+          {
+            label: "Our Vision",
+            translate: "MENU.ABOUT.VISION",
+            route: "/about",
+            queryParams: { section: "our-vision" },
+          },
+          {
+            label: "Invest in Nature",
+            translate: "MENU.ABOUT.INVEST",
+            route: "/about",
+            queryParams: { section: "invest-in-nature" },
+          },
+          {
+            label: "Ecosystems",
+            translate: "MENU.ABOUT.ECOSYSTEMS",
+            route: "/about",
+            queryParams: { section: "ecosystems" },
+          },
+          {
+            label: "Metrics",
+            translate: "MENU.ABOUT.METRICS",
+            route: "/about",
+            queryParams: { section: "metrics" },
+          },
+        ],
+      },
+      {
+        id: "community",
+        label: "Community",
+        translate: "MENU.COMMUNITY.ROOT",
+        route: "/community",
+        children: [
+          {
+            route: "/community",
+            translate: "MENU.COMMUNITY.NETWORK",
+            label: "The Network",
+            queryParams: { section: "network" },
+          },
+          {
+            route: "/community",
+            translate: "MENU.COMMUNITY.LABORATORIES",
+            label: "Laboratories",
+            queryParams: { section: "laboratories" },
+          },
+          {
+            route: "/community",
+            translate: "MENU.COMMUNITY.FORUM",
+            label: "Forum",
+            queryParams: { section: "forum" },
+          },
+        ],
+      },
+      {
+        id: "technology",
+        label: "Technology",
+        translate: "MENU.TECHNOLOGY.ROOT",
+        route: "/technology",
+        children: [
+          {
+            route: "/technology",
+            translate: "MENU.TECHNOLOGY.EDNA",
+            label: "eDNA",
+            queryParams: { section: "edna" },
+          },
+          {
+            route: "/technology/",
+            translate: "MENU.TECHNOLOGY.REMOTE_SENSING",
+            label: "Remote sensing",
+            queryParams: { section: "remote-sensing" },
+          },
+          {
+            route: "/technology",
+            translate: "MENU.TECHNOLOGY.PROTOCOLS",
+            label: "Protocols",
+            queryParams: { section: "protocols" },
+          },
+          {
+            route: "/technology",
+            translate: "MENU.TECHNOLOGY.BIOBANK",
+            label: "Biobank",
+            queryParams: { section: "biobank" },
+          },
+        ],
+      },
+      {
+        id: "resources",
+        label: "Resources",
+        translate: "MENU.RESOURCES.ROOT",
+        route: "/resources",
+        children: [
+          {
+            route: "/resources",
+            translate: "MENU.RESOURCES.DATA_SHARING",
+            label: "Data sharing",
+            queryParams: { section: "data-sharing" },
+          },
+          {
+            route: "/resources",
+            translate: "MENU.RESOURCES.SINEWS",
+            label: "Resources",
+            queryParams: { section: "sinews" },
+          },
+          {
+            route: "/resources",
+            translate: "MENU.RESOURCES.PUBLICATIONS",
+            label: "Publications",
+            queryParams: { section: "publications" },
+          },
+        ],
+      },
+      {
+        id: "actions",
+        label: "Actions",
+        translate: "MENU.ACTIONS.ROOT",
+        route: "/actions",
+        children: [
+          {
+            route: "/actions",
+            translate: "MENU.ACTIONS.ENDANGERED",
+            label: "Endangered species actions",
+            queryParams: { section: "endangered" },
+          },
+          {
+            route: "/actions",
+            translate: "MENU.ACTIONS.EFFECTIVENESS",
+            label: "Effectiveness of protected areas",
+            queryParams: { section: "effectiveness" },
+          },
+          {
+            route: "/actions",
+            translate: "MENU.ACTIONS.RESTORATION",
+            label: "Restoration and rewilding",
+            queryParams: { section: "restoration" },
+          },
+          {
+            route: "/actions",
+            translate: "MENU.ACTIONS.CORRIDORS",
+            label: "Corridors and connectivity",
+            queryParams: { section: "corridors" },
+          },
+          {
+            route: "/actions",
+            translate: "MENU.ACTIONS.MONITORING",
+            label: "Monitoring of genetic diversity",
+            queryParams: { section: "monitoring" },
+          },          {
+            route: "/actions",
+            translate: "MENU.ACTIONS.LONG_TERM",
+            label: "Long term impact assessment",
+            queryParams: { section: "long-term" },
+          },
+        ],
+      },
+    ]);
+  } catch (e) {
+    return res.status(500).json(error.message);
+  }
 });
 
 module.exports = router;
